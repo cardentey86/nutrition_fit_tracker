b3720c8615b7bfa17e2c3afb57157e0c015d9c00
Adrian <adrianalvarezcardentey@gmail.com>
2025-03-01 20:09:26 -0500

    update create database and init on launch apk

--- lib/data/database_helper.dart
+++ lib/data/database_helper.dart
@@ -23,60 +23,35 @@ class DatabaseHelper {
     return await openDatabase(
       path,
       onCreate: (db, version) {
-        return db.execute(
+        // Crear tabla de alimentos
+        db.execute(
           'CREATE TABLE alimentos(id INTEGER PRIMARY KEY, Alimento TEXT, Calorias INTEGER, Carbohidratos REAL, Proteinas REAL, Grasas REAL, Fibra REAL)',
         );
+
+        // Crear tabla de medidas personales
+        db.execute(
+          'CREATE TABLE MedidasPersonales(Id INTEGER PRIMARY KEY, Fecha TEXT, Pecho REAL, Cintura REAL, Cadera REAL, Muslo REAL, Pantorrilla REAL, Biceps REAL, Antebrazo REAL, Muneca REAL, Cuello REAL, Tobillo REAL)',
+        );
+
+        // Crear tabla de menús
+        db.execute(
+          'CREATE TABLE Menu(Id INTEGER PRIMARY KEY, Nombre TEXT)',
+        );
+
+        // Crear tabla de platos en menús
+        db.execute(
+          'CREATE TABLE MenuPlato(Id INTEGER PRIMARY KEY, IdMenu INTEGER, IdAlimento INTEGER, Fecha TEXT, FOREIGN KEY(IdMenu) REFERENCES Menu(Id), FOREIGN KEY(IdAlimento) REFERENCES alimentos(id))',
+        );
       },
+      /* onUpgrade: (db, oldVersion, newVersion) {  
+        if (oldVersion < 2) {  
+          // Cambiar la estructura si estás actualizando a la versión 2  
+          db.execute('ALTER TABLE alimentos ADD COLUMN NuevaColumna REAL');   
+        }  
+        // Puedes agregar más condiciones para versiones posteriores  
+      },  
+      version: 2, // Incrementa la versión aquí   */
       version: 1,
     );
   }
-
-  /* Future<void> insertAlimento(Alimento alimento) async {
-    final db = await database;
-    await db.insert(
-      'alimentos',
-      alimento.toMap(),
-      conflictAlgorithm: ConflictAlgorithm.replace,
-    );
-  }
-
-  Future<List<Alimento>> getAlimentos() async {
-    final db = await database;
-    final List<Map<String, dynamic>> maps =
-        await db.query('alimentos', orderBy: 'Alimento');
-
-    return List.generate(maps.length, (i) {
-      return Alimento(
-        nombre: maps[i]['Alimento'],
-        calorias: (maps[i]['Calorias'] is int)
-            ? (maps[i]['Calorias'] as int).toDouble()
-            : (maps[i]['Calorias'] as double),
-        carbohidratos: (maps[i]['Carbohidratos'] is int)
-            ? (maps[i]['Carbohidratos'] as int).toDouble()
-            : (maps[i]['Carbohidratos'] as double),
-        proteinas: (maps[i]['Proteinas'] is int)
-            ? (maps[i]['Proteinas'] as int).toDouble()
-            : (maps[i]['Proteinas'] as double),
-        grasas: (maps[i]['Grasas'] is int)
-            ? (maps[i]['Grasas'] as int).toDouble()
-            : (maps[i]['Grasas'] as double),
-        fibra: (maps[i]['Fibra'] is int)
-            ? (maps[i]['Fibra'] as int).toDouble()
-            : (maps[i]['Fibra'] as double),
-      );
-    });
-  }
-
-  Future<bool> any() async {
-    final db = await database;
-    final List<Map<String, dynamic>> result =
-        await db.rawQuery('SELECT COUNT(*) AS count FROM alimentos');
-    int count = Sqflite.firstIntValue(result)!;
-    return count > 0;
-  }
-
-  Future<void> clearAlimentos() async {
-    final db = await database;
-    await db.delete('alimentos');
-  } */
 }
--- lib/modules/layout/screens/home_screen.dart
+++ lib/modules/layout/screens/home_screen.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:nutrition_fit_traker/data/database_helper.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final DatabaseHelper _helper = DatabaseHelper();
+
+  @override
+  void initState() {
+    super.initState();
+    _helper.database.then((db) {}).catchError((error) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text("Error on init database"),
+        ));
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
